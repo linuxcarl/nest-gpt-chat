c7effb6cc5a1fc49a86308dad0f74e4e02e5cbf6
Carlos Ramirez <carlos@extrasistemas.com>
2024-02-24 23:19:17 -0600

    feat: add modifications on orthography use case

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,6 +1,5 @@
 import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 import OpenAI from 'openai';
-import { json } from 'stream/consumers';
 
 interface Options {
   prompt: string;
@@ -10,25 +9,32 @@ export const orthographyCheckUseCase = async (
   openIa: OpenAI,
   options: Options,
 ) => {
+  const completion = await openIa.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content:
+          // 'Actua como un medico especializado en regeneracion celular, intermitente, suplementos epigeneticos y con conocimiento avanzado en enfermendades cronicas degenerativas. Tu nombre es Dra. Lucy',
+          'Necesito que apliques correccion ortografica del texto que se te enviara . Debes validar que las palabreas existan en el diccionario español y que esten escritas correctamente, asi como tambien validar puntuaciones, acentos, comas y signos.Recuerda quitar puntos por cada detalle, y SIEMPRE debes de contestar en json con el siguiente formato: {"userScore": number, "errors":["error=>solucion"], "message":string, "corrections":string}. En corrections dame el mismo  texto que se te envio pero ya coregido. Si todo esta bien con el texto enviado, contesta con el mismo formato json. userScore el valor maximo siempre sera 100',
+      },
+      {
+        role: 'user',
+        content: options.prompt,
+      },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
   try {
-    const completion = await openIa.chat.completions.create({
-      messages: [
-        {
-          role: 'system',
-          content:
-            // 'Actua como un medico especializado en regeneracion celular, intermitente, suplementos epigeneticos y con conocimiento avanzado en enfermendades cronicas degenerativas. Tu nombre es Dra. Lucy',
-            'Necesito que apliques correccion ortografica del texto que se te enviara . Debes validar que las palabreas existan en el diccionario español y que esten escritas correctamente. y debes de contestar en json con el siguiente formato: {userScore: number; errors:["error"=>"solucion"], messages:string}',
-        },
-        {
-          role: 'user',
-          content: options.prompt,
-        },
-      ],
-      model: 'gpt-3.5-turbo',
-    });
     const response = JSON.parse(completion.choices[0].message.content);
     return response;
   } catch (error) {
-    throw new ExceptionsHandler(error);
+    const message = completion.choices[0].message.content;
+    return {
+      message,
+      errors: [],
+      userScore: 0,
+      corrections: '',
+      hasDude: true,
+    };
   }
 };
