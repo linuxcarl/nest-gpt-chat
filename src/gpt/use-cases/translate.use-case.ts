225d905cfe25e1665b533dec6429802fd8062980
Carlos Ramirez <carlos@extrasistemas.com>
2024-02-26 22:57:51 -0600

    fix: response object translate to message

--- src/gpt/use-cases/translate.use-case.ts
+++ src/gpt/use-cases/translate.use-case.ts
@@ -20,5 +20,6 @@ export const translateUseCase = async (openIa: OpenAI, options: Options) => {
     ],
     model: 'gpt-3.5-turbo',
   });
-  return completion.choices[0].message;
+  const message = completion.choices[0].message.content;
+  return { message };
 };
